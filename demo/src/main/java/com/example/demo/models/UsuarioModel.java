5888cad1105e09cf7b0a0f7fba1230c85dc730a9
Gerald2309 <68652749+Gerald2309@users.noreply.github.com>
2022-02-18 09:46:46 -0500

    Conexión a bases de datos y creación de la tabla usuario

---
+++ demo/src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,50 @@
+package com.example.demo.models;
+
+import javax.persistence.*; //importa todas las anotaciones de JPA
+
+@Entity
+@Table(name = "usuario") //Nombre de la tabla, si no se pone queda por defecto el nombre de la clase
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //se genera y aumenta automáticamente
+    @Column(unique = true, nullable = false) //NOT NULL
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+    public void setPrioridad(Integer prioridad){
+        this.prioridad = prioridad;
+    }
+
+    public Integer getPrioridad(){
+        return prioridad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
